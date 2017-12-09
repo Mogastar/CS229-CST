67b08c473dec122b724c67f35d591f0a79ba3e69
Kevin Chen <kchen42@stanford.edu>
2017-12-08 19:05:33 -0800

    Data and value

--- project.py
+++ project.py
@@ -299,7 +299,7 @@ def Reg_nS_Deltat(score, time, nbins = 1000, tol = 1e-5):
     return reg, score_picks_log, time_picks, time_sorted, score_sorted
 
 
-def separate(val, row, col, y_list, test_size, seed = 0):
+def separate(val, row, col, y, test_size, seed = 0):
     '''
     Separate a sparse matrix in COO format and a vector y into 2 sets
         - set 0 of size (1 - test_size) %
@@ -307,7 +307,7 @@ def separate(val, row, col, y_list, test_size, seed = 0):
     '''
     
     # Separate indices
-    ind = np.arange(len(y_list[0]))
+    ind = np.arange(y.shape[0])
     ind0, ind1 = sk.model_selection.train_test_split(ind, 
                                                      test_size = test_size, 
                                                      random_state = seed)
@@ -345,13 +345,10 @@ def separate(val, row, col, y_list, test_size, seed = 0):
     col1 = np.array(f1(col))
     
     # Separate y
-    y_list0 = []
-    y_list1 = []
-    for y in y_list:
-        y_list0.append(y[ind0])
-        y_list1.append(y[ind1])
+    y0 = y[ind0, :]
+    y1 = y[ind1, :]
     
-    return val0, row0, col0, y_list0, val1, row1, col1, y_list1
+    return val0, row0, col0, y0, val1, row1, col1, y1
 
 
 '''
@@ -395,30 +392,28 @@ val, row, col = aggregate_design(work_dir)
 # Vectors to split
 accepted = np.array(df['IsAcceptedAnswer'], dtype = int)
 deltaT_s = np.array(df['DeltaT'].dt.total_seconds())
-y_list = [accepted, deltaT_s]
+bodylength = np.array(df['Bodylength_std'])
+y = np.stack((accepted, deltaT_s, bodylength), axis = 1)
 
 # Separate datasets
-val_temp, row_temp, col_temp, y_list_temp, val_test, row_test, col_test, y_list_test = \
-    separate(val, row, col, y_list, test_size = 0.01)
-val_train, row_train, col_train, y_list_train, val_cv, row_cv, col_cv, y_list_cv = \
-    separate(val_temp, row_temp, col_temp, y_list_temp, test_size = 0.1)
-del val_temp, row_temp, col_temp, y_list_temp
+val_temp, row_temp, col_temp, y_temp, val_test, row_test, col_test, y_test = \
+    separate(val, row, col, y, test_size = 0.01)
+val_train, row_train, col_train, y_train, val_cv, row_cv, col_cv, y_cv = \
+    separate(val_temp, row_temp, col_temp, y_temp, test_size = 0.1)
+del val_temp, row_temp, col_temp, y_temp
 
 # Construct sparse matrices
-accepted_train = y_list_train[0]
-deltaT_s_train = y_list_train[1]
 X_train = scipy.sparse.coo_matrix((val_train, (row_train, col_train)),
-                                  shape = (len(accepted_train), len(voc)))
-del val_train, row_train, col_train
-accepted_cv = y_list_cv[0]
-deltaT_s_cv = y_list_cv[1]
+                                  shape = (y_train.shape[0], len(voc)))
 X_cv = scipy.sparse.coo_matrix((val_cv, (row_cv, col_cv)),
-                                  shape = (len(accepted_cv), len(voc)))
-del val_cv, row_cv, col_cv
-accepted_test = y_list_test[0]
-deltaT_s_test = y_list_test[1]
+                                  shape = (y_cv.shape[0], len(voc)))
 X_test = scipy.sparse.coo_matrix((val_test, (row_test, col_test)),
-                                 shape = (len(accepted_test), len(voc)))
+                                 shape = (y_test.shape[0], len(voc)))
+
+# Delete unused shit
+del val, row, col
+del val_train, row_train, col_train
+del val_cv, row_cv, col_cv
 del val_test, row_test, col_test
 
 
@@ -429,23 +424,29 @@ Tests
 '''
 
 
-# Tests
+# NB
 
-MNB = MultinomialNB()
-MNB.fit(X_train, accepted_train)
-y_MNB = MNB.predict(X_cv)
-proba_MNB = MNB.predict_proba(X_cv)
-accuracy_MNB = np.mean(y_MNB == accepted_cv)
-print(accuracy_MNB)
+bernoulli = True
 
-BNB = BernoulliNB()
-BNB.fit(X_train, accepted_train)
-y_BNB = BNB.predict(X_cv)
-proba_BNB = BNB.predict_proba(X_cv)
-accuracy_BNB = np.mean(y_BNB == accepted_cv)
-print(accuracy_BNB)
+if bernoulli:
+    NB = BernoulliNB()
+else:
+    NB = MultinomialNB()
+NB.fit(X_train, y_train[:, 0])
 
-reg, score_log, time_picks, time_sorted, score_sorted = Reg_nS_Deltat(df['Score_std'], df['DeltaT'], 5000)
+# Predictions 
+
+pred_NB_train = NB.predict(X_train)
+proba_NB_train = NB.predict_proba(X_train)
+pred_NB_cv = NB.predict(X_cv)
+proba_NB_cv = NB.predict_proba(X_cv)
+pred_NB_test = NB.predict(X_test)
+proba_NB_test = NB.predict_proba(X_test)
+
+# Plot enveloppe
+
+reg, score_log, time_picks, time_sorted, score_sorted = \
+    Reg_nS_Deltat(df['Score_std'], df['DeltaT'], 5000)
 pred = np.exp(reg.predict(time_picks))
 plt.plot(time_sorted, score_sorted, 'x')
 plt.plot(time_picks, pred, 'r-')
@@ -458,6 +459,16 @@ plt.ylabel('Standardized score')
 plt.title('Regression on time difference and standardized score')
 plt.savefig("regression_Time_Score.png", dpi=1200)
 
-enveloppe_cv = reg.predict(deltaT_s_cv.reshape(len(deltaT_s_cv), 1))
+# Logistic regression
 
+envlp_train = np.exp(reg.predict(y_train[:, 1].reshape(y_train.shape[0], 1)))
+envlp_cv = np.exp(reg.predict(y_cv[:, 1].reshape(y_cv.shape[0], 1)))
+envlp_test = np.exp(reg.predict(y_test[:, 1].reshape(y_test.shape[0], 1)))
 
+data_train = np.stack((pred_NB_train, envlp_train.flatten(), y_train[:, 2]), axis = 1)
+data_cv = np.stack((pred_NB_cv, envlp_cv.flatten(), y_cv[:, 2]), axis = 1)
+data_test = np.stack((pred_NB_test, envlp_test.flatten(), y_test[:, 2]), axis = 1)
+
+value_train = y_train[:, 0]
+value_cv = y_cv[:, 0]
+value_test = y_test[:, 0]
